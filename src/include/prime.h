5348ee1693196a2f383e7441a235c259d95fcd32
Spandan Roy <royspandan1@gmail.com>
2026-08-02 18:00:02 +0530

    Import 10000 reimann zeros

--- src/include/prime.h
+++ src/include/prime.h
@@ -4,10 +4,10 @@
 #include "num_vec.h"
 #include <stdbool.h>
 
-void            prime_init(void);
-void            prime_free(void);
-bool            is_prime(int n);
-void            sieve(int ceiling);
+void prime_init(void);
+void prime_free(void);
+bool is_prime(int n);
+void sieve(int ceiling);
 const num_vec_t* prime_list(void);
 
 #endif // PRIME_H
